717d231b8d1948e499b49b7e2c1921b3c9b4c9eb
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-09-29 23:22:29 -0400

    assign useRef for MealItemForm, wrap Input with React.forwardRef in order to get an access to current.value, implement validation for entered amount, add error message if input value is invalid using useState, implement logic for a valid input

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = ({ name, description, price, id }) => {
+  const cartCtx = useContext(CartContext);
   const priceRefactoring = `$ ${price.toFixed(2)}`;
+
+  const onAddToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: id,
+      name: name,
+      amount: amount,
+      price: price,
+    });
+  };
   return (
     <li className={styles.meal}>
       <div>
@@ -12,7 +23,7 @@ const MealItem = ({ name, description, price, id }) => {
         <div className={styles.price}>{priceRefactoring}</div>
       </div>
       <div>
-        <MealItemForm id={id} />
+        <MealItemForm id={id} onAddToCart={onAddToCartHandler} />
       </div>
     </li>
   );
