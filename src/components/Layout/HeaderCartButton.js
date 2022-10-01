387d8b769fa8015ebd6131b13818df405ed71d33
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-10-01 00:35:14 -0400

    add useEffect to handle bump button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = ({ onClick }) => {
+  const [btnIsHiglighted, setBtnIsHiglighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
-  const numberOfCartItems = cartCtx.items.reduce((currNum, item) => {
+  const numberOfCartItems = items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
 
+  const btnClasses = `${styles.button} ${btnIsHiglighted ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setBtnIsHiglighted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHiglighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
