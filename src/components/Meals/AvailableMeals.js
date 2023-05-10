373ba9404cc1ecdf696eda4b386fa3690591c86f
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-05-09 23:10:55 -0400

    add loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,6 +5,7 @@ import MealItem from './MealItem/MealItem';
 
 const AvailableMeals = () => {
   const [allMeals, setAllMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
 
   const fetchMeals = async () => {
     const response = await fetch(
@@ -14,12 +15,18 @@ const AvailableMeals = () => {
     const data = await response.json();
     console.log("IM GETTING DATA", data);
     setAllMeals(data);
+    setIsLoading(false)
   };
 
   useEffect(() => {
     fetchMeals();
   }, []);
 
+  if(isLoading){
+    return <section className={styles.MealsLoading}>
+      <p>Loading...</p>
+    </section>
+  }
   const mealsList = allMeals.map((meal) => (
     <MealItem
       id={meal.id}
@@ -29,6 +36,8 @@ const AvailableMeals = () => {
       description={meal.description}
     />
   ));
+
+
   return (
     <section className={styles.meals}>
       <Card>
