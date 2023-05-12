cef76802fdeb79e7a04d278569bf5ef7ed276798
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-05-11 22:32:17 -0400

    add reset functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, Fragment } from 'react';
 import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import styles from './Cart.module.css';
@@ -7,6 +7,8 @@ import Checkout from './Checkout';
 
 const Cart = ({ onClose }) => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 
@@ -24,17 +26,22 @@ const Cart = ({ onClose }) => {
     setIsCheckout(true);
   };
 
-  const submitOrderHandler = (userData) => {
-    fetch('https://foodyapp-44e91-default-rtdb.firebaseio.com/orders.json', {
-      method: 'POST',
-      body: JSON.stringify({
-        user: userData,
-        orderedItems: cartCtx.items,
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(
+      'https://foodyapp-44e91-default-rtdb.firebaseio.com/orders.json',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          user: userData,
+          orderedItems: cartCtx.items,
+        }),
+      }
+    );
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
+
   };
 
   const cartItems = (
@@ -64,15 +71,37 @@ const Cart = ({ onClose }) => {
       )}
     </div>
   );
-  return (
-    <Modal onClose={onClose}>
+
+  const cartModalContent = (
+    <Fragment>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={onClose} />
+      )}
       {!isCheckout && modalActions}
+    </Fragment>
+  );
+
+  const isSubmittingModalContent = <p>Sending order data...</p>;
+  const didSubmitModalContent = (
+    <Fragment>
+      <p>Successfully sent the order!</p>
+      <div className={styles.actions}>
+        <button onClick={onClose} className={styles.button}>
+          Close
+        </button>
+      </div>
+    </Fragment>
+  );
+  return (
+    <Modal onClose={onClose}>
+      {!isSubmitting && !didSubmit && cartModalContent}
+      {isSubmitting && isSubmittingModalContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 };
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -11,7 +11,7 @@ const cartReducer = (state, action) => {
     // avoiding mutation a current array to add a new item to the cart list using concat method in order to generate a brand new array
 
     const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id,
+      (item) => item.id === action.item.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
 
@@ -35,7 +35,9 @@ const cartReducer = (state, action) => {
     };
   }
   if (action.type === 'REMOVE_CART_ITEM') {
-    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
 
     const existingCartItem = state.items[existingCartItemIndex];
     const updatedTotalAmount = state.totalAmount - existingCartItem.price;
@@ -43,16 +45,22 @@ const cartReducer = (state, action) => {
     if (existingCartItem.amount === 1) {
       updatedItems = state.items.filter((item) => item.id !== action.id);
     } else {
-      const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1 };
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
     }
-
 
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
+  }
+
+  if (action.type === 'CLEAR') {
+    return defaultCartState;
   }
   return defaultCartState;
 };
@@ -60,7 +68,7 @@ const cartReducer = (state, action) => {
 const CartProvider = (props) => {
   const [cartState, dispatchCartAction] = useReducer(
     cartReducer,
-    defaultCartState,
+    defaultCartState
   );
   const addItemToCartHandler = (item) => {
     dispatchCartAction({ type: 'ADD_CART_ITEM', item: item });
@@ -70,11 +78,16 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: 'REMOVE_CART_ITEM', id: id });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: 'CLEAR' });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return (
